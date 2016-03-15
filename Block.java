9085826d023afd190f550d5c24c4d8138abd49c4
hkp93 <henvy.patel@outlook.com>
2016-03-15 00:10:27 -0500

    gui_block/unblock
    Gui front-end done:block/unblock works successfully
    incorporation of encrypt/decrypt , key remaining

--- Block.java
+++ Block.java
@@ -1,7 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Block.java - This class has one sole purpose, it needs to be able to take a file and block it to
  * a specified block size.  It also needs to do the inverse of said operation and it needs to be
- * able to unblock a file as well.  This operation is offered to the user as a sole operation, but 
+ * able to unblock a file as well which is implemented in Unblock.java.  This operation is offered to the user as a sole operation, but 
  * is also implemented within the encryption and decryption algorithm within the Encryption.java
  * class.
  * @version     1.0.0
@@ -15,11 +21,145 @@
  */
 public class Block {
 
-	/**
-	 * 
-	 */
-	protected Block () {
-
+	
+	// gets the file name
+	private String fileName;
+	private File oFile;
+	// block size of data
+	int blocksize ;
+	private String storeMsg;
+	
+	//constructor for the block
+	public Block (String inputFile, int val, String outputFile) throws IOException {
+		
+		// make the text file here
+		if(inputFile.endsWith("txt") != true){
+			
+		    fileName = inputFile.concat(".txt");
+		}
+        if(outputFile.endsWith("txt") != true){
+			
+		    outputFile = outputFile.concat(".txt");
+		}
+         oFile = new File(outputFile);
+		blocksize = val*2;
+		storeMsg = "";
+		// method to convert the message
+		if(fileName != null){
+			System.out.println(fileName);
+			makeMessage();
+			writeMsg();
+		}
+		else{
+			System.out.println("No such file found!!");
+			return;
+		}
+	}
+	
+	
+	// method to convert message to the numeric
+	public void makeMessage() throws IOException{
+		
+		FileReader inputfile = new FileReader(fileName);
+		// variable to store each read from the file
+		int readval;
+	
+		try {
+			while((readval = inputfile.read()) != -1){
+				
+				// check if a special arr
+				String temp = "";
+				if(spl_ascii(readval,temp)== 1){
+					System.out.println(temp + readval);
+					storeMsg = storeMsg +temp;
+				}
+				else{
+					int val = readval - 27;
+					// if the val is less 
+					if(val < 10){
+						temp = "0"+ Integer.toString(val);
+					}
+					else{
+						readval = readval-27;
+						temp = Integer.toString(readval);
+					}
+					// put combine the string
+					storeMsg = storeMsg +temp;
+					System.out.println(storeMsg);
+				}
+			}
+		}
+		catch(IOException e){
+			// catch the error
+			System.out.println("Error in message converting");
+		}
+	}
+	
+	
+	// method to replace the special ascii characters
+	public int spl_ascii(int val , String s){
+		if(val == 0){
+			s = "00";
+			return 1;
+		}
+		else if(val == 11){
+			s = "01";
+			return 1;
+		}
+		else if(val == 9){
+			s = "02";
+			return 1;
+		}
+		else if(val == 10){
+			s = "03";
+			return 1;
+		}
+		else if(val == 13){
+			s = "04";
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	// to wrtite a msg in the new file
+	public void writeMsg() throws IOException{
+		// steps to divid according to block soze
+		FileWriter newFile ;
+		newFile = new FileWriter(oFile);
+		BufferedWriter w ;
+		w =  new BufferedWriter(newFile);
+		int strlen = storeMsg.length();
+		// place hlder for holding the string length
+		int hold = storeMsg.length();
+		System.out.println("intial lengh:"+strlen);
+		int c = 0;
+		int range = blocksize;
+		while(strlen > blocksize){
+			w.write(storeMsg.substring(c,range+c));
+			w.newLine();
+			c= c+blocksize;
+			strlen=strlen-blocksize;
+			System.out.println(strlen);
+		}
+		System.out.println(strlen);
+		
+		
+		// mean padding requires
+		if(strlen > 0){
+			System.out.println("after adding"+strlen);
+			int left = blocksize - strlen;
+			String leftstr = storeMsg.substring(hold-strlen);
+			System.out.println("string left to copoy"+strlen);
+			System.out.println(left);
+			for(int i = 0 ;i<left;i++){
+				leftstr = "0"+ leftstr;
+			}
+			// add the leftover
+			w.write(leftstr);
+		}
+		w.close();
 	}
 
 }
--- GUI.java
+++ GUI.java
@@ -14,10 +14,296 @@
  * @author      Henvy Patel
  * @license     GNU Public License <http://www.gnu.org/licenses/gpl-3.0.txt>
  */
-public class GUI {
 
-	protected GUI () {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GUI extends JFrame implements ActionListener{
+	
+		// JLabels
+		private JLabel forTitle;
+		private JLabel forTarget;
+		private JLabel forBlocking;
+		private JLabel forbfile;
+		private JLabel forlfile;
+		//labels for unblocking
+		private JLabel uforbfile;
+		private JLabel uforlfile;
+		// JButtons for various options
+		public JLabel toKeystatus;
+		private JLabel blocksize;
+		private JLabel blocksize2;
+		
+		// various buttons to perform various function
+		private MyJButton toCreate;
+		private MyJButton toLoad;
+		private MyJButton toOpen;
+		private MyJButton toBlock;
+		private MyJButton toUnblock;
+		private MyJButton toEncrypt;
+		private MyJButton toDecrypt;
+		
+		// Text field for getting the filepath
+		private JTextField filepath;
+		private JTextField fileloc;
+		private JTextField bsize1;
+		private JTextField bsize2;
+		// file inputs for unblock
+		public JTextField ufilepath;
+		public JTextField ufileloc;
+		// grid layout
+		private GridLayout grid;
+		
+		// toogle is to keep track for events
+		private boolean toggle = true;
+		
+		// panels for adding various option i.e buttons, labels and text feild
+		private JPanel options;
+		private JPanel koptions;
+		private JPanel target;
+		private JPanel blocking;
+		private JPanel unblocking;
+		private JPanel encrypt;
+		
+		
+		// declaring a container to add all the elements of the GUI
+		private Container container;
+		
+		
+		// Constructor for GUI
+		public GUI(){
+			
+			super("RSA Encrypion");
+			
+			// intializr grids
+			grid = new GridLayout( 7, 1);
+			// create new JPanels for each options in the RSA program
+			options = new JPanel();
+			koptions = new JPanel();
+			target  = new JPanel();
+			//target.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+
+			blocking = new JPanel();
+			unblocking = new JPanel();
+			encrypt   = new JPanel();
+			
+			// intializing the container
+			container = getContentPane();
+			container.setLayout(grid);
+			
+			// for the title
+			forTitle = new JLabel("RSA Encryption Key Options:");
+			options.add(Box.createRigidArea(new Dimension(5,0)));  
+			options.add(forTitle);
+			
+			// Option 1 : Key Status & Type Options:
+			toKeystatus = new JLabel("Key Status & Type ");
+			koptions.add(Box.createRigidArea(new Dimension(5,0)));  
+			koptions.add(toKeystatus);
+			toCreate = new MyJButton("Create");
+			toCreate.setnum(1);
+			toCreate.addActionListener( this );
+			koptions.add(Box.createRigidArea(new Dimension(5,0)));  
+			koptions.add(toCreate);
+			toLoad = new MyJButton("Load");
+			toLoad.setnum(2);
+			toLoad.addActionListener( this );
+			koptions.add(Box.createRigidArea(new Dimension(5,0)));  
+			koptions.add(toLoad);
+		    
+			
+			// for the title target Files
+			forBlocking = new JLabel("Blocking:");
+			blocking.add(forBlocking,BorderLayout.PAGE_START);
+			
+			
+			// OPTION 2: Blocking
+			filepath  = new JTextField(10);
+			forbfile =  new JLabel("Source:");
+			forlfile =  new JLabel("Destination:");
+			fileloc = new JTextField(10);
+			blocksize = new JLabel("Block size:");
+			bsize1 = new JTextField(3);
+			blocking.add(forbfile);
+			blocking.add(filepath);
+			blocking.add(forlfile);
+			blocking.add(fileloc);
+			blocking.add(blocksize);
+			blocking.add(bsize1);
+			toBlock = new MyJButton("BLOCK");
+			toBlock.setnum(3);
+			toBlock.addActionListener( this );
+			blocking.add(toBlock);
+			
+			// OPTION 3:Unblocking
+			forTarget = new JLabel("Unblocking:");
+			unblocking.add(forTarget);
+			ufilepath  = new JTextField(10);
+			uforbfile =  new JLabel("Source:");
+			uforlfile =  new JLabel("Destination:");
+			blocksize2 = new JLabel("Block size:");
+			bsize2 = new JTextField(3);
+			ufileloc = new JTextField(10);
+			unblocking.add(uforbfile);
+			unblocking.add(ufilepath);
+			unblocking.add(uforlfile);
+			unblocking.add(ufileloc);
+			unblocking.add(blocksize2);
+			unblocking.add(bsize2);
+			unblocking.add(Box.createRigidArea(new Dimension(2,0))); 
+			toUnblock = new MyJButton("UNBLOCK");
+			toUnblock.setnum(4);
+			toUnblock.addActionListener( this );
+			unblocking.add(toUnblock);
+			
+			// option 4 : encrypt decrpt
+			toEncrypt = new MyJButton("ENCRYPT");
+			toEncrypt.setnum(5);
+			toEncrypt.addActionListener( this );
+			encrypt.add(toEncrypt);
+		    toDecrypt = new MyJButton("DECRYPT");
+		    toDecrypt.setnum(6);
+			toDecrypt.addActionListener( this );
+		    encrypt.add(toDecrypt);
+		    
+			
+			//adding the panels to the container
+			
+			container.add(options);
+			container.add(koptions);
+			container.add(target);
+			container.add(blocking);
+			container.add(unblocking);
+			container.add(encrypt);
+			setSize( 600, 450 );
+		    setVisible( true );
+			
+	}// end Of Gui Cnstrutor
+	
+	
+	// implementation of ActionListner
+	public void actionPerformed (ActionEvent e)
+	  {
+		// here the v will be the values that will be passes to represent each button and it funtion
+		MyJButton temp = (MyJButton) e.getSource();
+		
+		if(temp.getnum() == 1){
+			// do create key
+		}
+		if(temp.getnum() == 2){
+			// do load key
+		}
+		if(temp.getnum() == 3){
+		
+			// do blocking
+			String inputfile = filepath.getText();
+			if(inputfile == null){
+				JOptionPane.showMessageDialog( this, "Please enter source file and try again");
+				return;
+			}
+			String outputfile = fileloc.getText();
+			if(outputfile == null){
+				JOptionPane.showMessageDialog( this, "Please enter Location to save file and try again");
+				return;
+			}
+			String val = bsize1.getText();
+			if(val == null){
+				JOptionPane.showMessageDialog( this, "Please enter block and try again");
+				return;
+			}
+			int val1 = Integer.parseInt(val);
+			try {
+				Block blockfile = new Block(inputfile, val1, outputfile);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			 filepath.setText("");
+			 fileloc.setText("");
+			 bsize1.setText("");
+			 JOptionPane.showMessageDialog( this, "Blocking sucessfully completed!");
+			
+
+		}
+		if(temp.getnum() == 4){
+			// do unblocking
+			// do blocking
+			String inputfile = ufilepath.getText();
+			if(inputfile == null){
+				JOptionPane.showMessageDialog( this, "Please enter source file and try again");
+				return;
+			}
+			String outputfile = ufileloc.getText();
+			if(outputfile == null){
+				JOptionPane.showMessageDialog( this, "Please enter Location to save file and try again");
+				return;
+			}
+			String val = bsize2.getText();
+			if(val == null){
+				JOptionPane.showMessageDialog( this, "Please enter block size and try again");
+				return;
+			}
+			int val1 = Integer.parseInt(val);
+			try {
+				Unblock blockfile = new Unblock(inputfile, val1, outputfile);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			 ufilepath.setText("");
+			 ufileloc.setText("");
+			 bsize2.setText("");
+			 JOptionPane.showMessageDialog( this, "Unblocking sucessfully completed!");
+		}
+		if(temp.getnum() == 5){
+			// do encrypt
+		}
+		if(temp.getnum() == 6){
+			// do decrypt
+		}
+		
+	  }
+	
+	// MYJBUTTON TO ASSIGNE VALUES TO THE BUTTON TO PERFORM VARIOUS FUNCTIONs
+	class MyJButton extends JButton
+	{
+	  private int num;
+	  
+	  public MyJButton ( String text )
+	  {
+	    super (text);
+	    //setText (text);
+	  }
+	  
+	  
+	  public MyJButton ( String text , int n)
+	  {
+	    super (text);
+	    num = n;
+	  }
+	  
+	  public void setnum (int n)
+	  {
+	    num = n;
+	  }
+	  
+	  public int getnum ()
+	  {
+	    return num;
+	  }
 	}
+}// end of class Gui
 
-}
+
